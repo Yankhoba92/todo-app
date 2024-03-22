6982ac366eccb81941c767373fe67577eb53fc62
Yankhoba <bagayoko.yan@gmail.com>
2024-03-22 10:24:51 +0100

    🚧 creéation de mon service todos

---
+++ src/todos/todos.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { Todo } from './interfaces/todos.interface';
+
+@Injectable()
+export class TodosService {
+  todos: Todo[] = [
+    {
+      id: 1,
+      title: 'todos app',
+      description: 'Create NestJs app',
+      done: false,
+    },
+    {
+      id: 2,
+      title: 'bread',
+      description: 'buy bread',
+      done:true
+    },
+    {
+      id: 3,
+      title: 'wine',
+      description: 'buy wine',
+      done:true
+    },
+  ];
+  findAll(): Todo[]{
+    return this.todos
+  }
+}
